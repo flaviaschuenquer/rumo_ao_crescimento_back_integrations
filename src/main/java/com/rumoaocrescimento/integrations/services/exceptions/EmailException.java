5a18afd48151d7883ca56036e7a2c4c6a85cecfc
Flaiva Schuenquer <flavia_schuenquer@hotmail.com>
2022-01-03 22:12:32 -0300

    Sanding email, Exception handling

---
+++ src/main/java/com/rumoaocrescimento/integrations/services/exceptions/EmailException.java
@@ -0,0 +1,10 @@
+package com.rumoaocrescimento.integrations.services.exceptions;
+
+public class EmailException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public EmailException(String msg) {
+		super(msg);
+	}
+
+}
